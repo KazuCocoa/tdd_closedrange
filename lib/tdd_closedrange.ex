7e74a25eed7a110ffe62ee8893a973eee3646362
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-06-09 07:29:50 +0900

    1st

--- lib/tdd_closedrange.ex
+++ lib/tdd_closedrange.ex
@@ -1,2 +1,20 @@
 defmodule TddClosedrange do
+
+  @type t :: tuple
+
+  @type upper :: integer
+  @type lower :: integer
+
+  @spec range(upper, lower) :: {upper, lower}
+  def range(lower, upper), do: {lower, upper}
+
+  @spec lower(TddClosedrange.t) :: lower
+  def lower({lower, _}), do: lower
+
+  @spec upper(TddClosedrange.t) :: upper
+  def upper({_, upper}), do: upper
+
+  @spec to_string(TddClosedrange.t) :: String.t
+  def to_string({lower, upper}), do: "[#{lower},#{upper}]"
+
 end
--- test/tdd_closedrange_test.exs
+++ test/tdd_closedrange_test.exs
@@ -1,8 +1,20 @@
 defmodule TddClosedrangeTest do
   use ExUnit.Case
-  doctest TddClosedrange
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias TddClosedrange, as: TC
+
+  test "get lower bound" do
+    upper_bound = TC.range(1, 3) |> TC.lower
+    assert upper_bound == 1
+  end
+
+  test "get upper bound" do
+    upper_bound = TC.range(1, 3) |> TC.upper
+    assert upper_bound == 3
+  end
+
+  test "get upper string" do
+    upper_bound = TC.range(1, 3) |> TC.to_string
+    assert upper_bound == "[1,3]"
   end
 end
