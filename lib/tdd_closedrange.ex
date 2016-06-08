38aa2d642daf967f3a429cc583df7f8396ee21ae
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-06-09 08:05:28 +0900

    use only functions

--- lib/tdd_closedrange.ex
+++ lib/tdd_closedrange.ex
@@ -6,29 +6,20 @@ defmodule TddClosedrange do
   @type lower :: integer
 
   @spec range(lower, upper) :: {lower, upper}
-  def range(lower, upper) do
-    cond do
-      lower > upper -> raise(ArgumentError, "1st argument should lower than 2nd argument")
-      true          -> {lower, upper}
-    end
-  end
+  def range(lower, upper) when lower > upper, do: raise(ArgumentError, "1st argument should lower than 2nd argument")
+  def range(lower, upper),                    do: {lower, upper}
 
   @spec lower(TddClosedrange.t) :: lower
-  def lower({lower, _}), do: lower
+  def lower({lower, _upper}), do: lower
 
   @spec upper(TddClosedrange.t) :: upper
-  def upper({_, upper}), do: upper
+  def upper({_lower, upper}), do: upper
 
   @spec to_string(TddClosedrange.t) :: String.t
   def to_string({lower, upper}), do: "[#{lower},#{upper}]"
 
   @spec contain(TddClosedrange.t, integer) :: boolean
-  def contain({lower, upper}, value) do
-    cond do
-      value < lower -> false
-      value > upper -> false
-      true          -> true
-    end
-  end
-
+  def contain({lower, _upper}, value) when value < lower, do: false
+  def contain({_lower, upper}, value) when value > upper, do: false
+  def contain(_range, _value),                            do: true
 end
